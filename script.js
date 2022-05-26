5fe366e9577ce9795abc7e689efd17dc5e549471
sijan.ingrails <sijan.ingrails@gmail.com>
2022-05-26 13:22:53 +0545

    added random user ui and jhs

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
-fetch("https://randomuser.me/api/")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+let card = document.querySelector(".card");
+let count = 0;
+
+const getApi = () => {
+  fetch("https://randomuser.me/api?random=" + count)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      let results = data.results.map(({ name, email, ...info }) => {
+        // ({ name, cell, email, image, DOB, gender, location, login }) => {
+        /* console.log(name, cell, email);
+          console.log(name.first); */
+        console.log({ name, email, info });
+        card.innerHTML += ` <ul>
+        <div class="image">
+        <img src = ${info.picture.large} />
+        </div>
+        <div class="p-5 bg-white details flex flex-col gap-3">
+        <li class="name">
+        Name: ${name.title} ${name.first} ${name.last}
+           </li>
+           <li>
+           Age: ${info.registered.age} 
+           </li>
+   
+           <li class="phone">
+          Phone: ${info.cell}
+           </li>
+            <li class="email">
+           Email: <a href = mailto:${email} class = "underline" > ${email} </a> 
+            </li>
+            <li class =   "location">
+           Country: ${info.location.country}
+           </li>
+           <li>
+   
+           City: ${info.location.city}
+           </li>
+         
+            <li class="username">
+             Username: ${info.login.username}
+            </li>
+        </div>
+     
+       
+        </ul>
+        `;
+
+        // `My name is ${name}.`
+        // old method
+        // "My name is " + name + ". "
+      });
+      count += 1;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+var btn = document.querySelector(".btn");
+
+btn.addEventListener("click", getApi);
